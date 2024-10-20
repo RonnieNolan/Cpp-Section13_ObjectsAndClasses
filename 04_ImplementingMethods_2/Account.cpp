23cf4e21f0e03826074b9d6d0f6edea761fe2833
RonnieNolan <117862054+RonnieNolan@users.noreply.github.com>
2024-10-20 10:25:07 +0300

    Add files via upload

---
+++ 04_ImplementingMethods_2/Account.cpp
@@ -0,0 +1,34 @@
+#include "Account.h"
+//#include <string>
+
+//using namespace std;
+
+void Account::set_name(std::string n)
+{
+    name = n;
+}
+
+std::string Account::get_name()
+{
+    return name;
+}
+
+bool Account::deposit(double amount)
+{
+    // if verify amount
+    balance += amount;
+    return true;
+}
+
+bool Account::withdraw(double amount)
+{
+    if (balance - amount >= 0)
+	{
+        balance -= amount;
+        return true;
+    }
+	else
+	{
+        return false;
+    }
+}
