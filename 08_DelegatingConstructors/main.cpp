12ceb9e646c5c6a05807d061e16abcb2721e85b2
RonnieNolan <117862054+RonnieNolan@users.noreply.github.com>
2024-10-24 00:37:04 +0300

    Add files via upload

--- 08_DelegatingConstructors/main.cpp
+++ 08_DelegatingConstructors/main.cpp
@@ -17,19 +17,22 @@ public:
 };
 
 Player::Player() 
-    : Player {"None", 0, 0} {
+    : Player {"None", 0, 0}
+	{
         cout << "No-args constructor" << endl;
-}
+	}
 
 Player::Player(std::string name_val) 
-   : Player {name_val, 0, 0}  {
+   : Player {name_val, 0, 0}
+   {
            cout << "One-arg constructor" << endl;
-}
+	}
 
 Player::Player(std::string name_val, int health_val, int xp_val) 
-    : name{name_val}, health{health_val}, xp{xp_val} {
+    : name{name_val}, health{health_val}, xp{xp_val}
+	{
             cout << "Three-args constructor" << endl;
-}
+	}
 
 int main()
 {
